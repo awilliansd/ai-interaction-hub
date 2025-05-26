00f2cbd1b3fea95fe9ee8419f854d29d1a183278
Alessandro Dias <awilliansd@protonmail.com>
2025-05-26 12:14:27 -0300

    Nova verificação

--- github_count.py
+++ github_count.py
@@ -3,7 +3,7 @@ import requests
 # Configurações
 owner = "awilliansd"
 repo = "ai-interaction-hub"  # Apenas o nome do repositório
-tag = "v1.0.0"  # Tag da release
+tag = "v1.0.1"  # Tag da release
 url = f"https://api.github.com/repos/{owner}/{repo}/releases/tags/{tag}"
 
 # Cabeçalho para aceitar a versão correta da API
@@ -18,7 +18,6 @@ response = requests.get(url, headers=headers)
 # Verifica se a requisição foi bem-sucedida
 if response.status_code == 200:
     data = response.json()
-    print(data)  # Adicione esta linha para depuração
     if "assets" in data and data["assets"]:
         for asset in data["assets"]:
             print(f"Arquivo: {asset['name']}, Downloads: {asset['download_count']}")
